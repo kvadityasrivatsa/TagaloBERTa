e50b1579c13afaff6e2ec94de5b539201343f969
kvadityasrivatsa <k.v.aditya@research.iiit.ac.in>
2022-05-11 17:28:39 +0530

    updated proprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -17,17 +17,18 @@ def clean_label_df(df):
     return df
 
 def preprocess_label_data(datadf,split=False,test_size=None):
-    datadf = datadf.dropna()
+    # datadf = datadf.dropna()
     datadf = clean_label_df(datadf)
     datadf['text'] = [str(s)[:512].strip() for s in datadf['text']]
-    datadf['labels'] = [1 if int(l)==1 else 0 for l in datadf['labels']]
-    datadf = datadf[[len(l)>5 for l in datadf['text']]]
-
-    print(f'before pruning: {len(datadf)}')
-    datadf = datadf.drop_duplicates(subset=['text'],keep='first')
-    print(f'after pruning: {len(datadf)}')
+    if 'labels' in datadf.columns:
+        datadf['labels'] = [1 if int(l)==1 else 0 for l in datadf['labels']]
 
     if split:
+        datadf = datadf[[len(l)>5 for l in datadf['text']]]
+        print(f'before pruning: {len(datadf)}')
+        datadf = datadf.drop_duplicates(subset=['text'],keep='first')
+        print(f'after pruning: {len(datadf)}')
+
         traindf, testdf = train_test_split(datadf,test_size=test_size)
         traindf = traindf.reset_index(drop=True)
         testdf = testdf.reset_index(drop=True)
