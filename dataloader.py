5b94a59d3fa171a597e285279a37b263dadce26e
kvadityasrivatsa <k.v.aditya@research.iiit.ac.in>
2022-05-13 17:41:54 +0530

    minor bug fix 7

--- dataloader.py
+++ dataloader.py
@@ -16,7 +16,7 @@ base_model_urls = {'TagaloBERTa_1M':'https://drive.google.com/uc?id=199_fviLlads
 
 finetuned_model_urls = {'TagaloBERTa_RoBERTa_Bi10_30M.model':'https://drive.google.com/uc?id=1SiOXy7ma1nQckQKJ249pNnSzFh8FmhCE',
                         'TagaloBERTa_Bi20_30M.model':'https://drive.google.com/uc?id=1K1e9H8MV0uTGbVal4cZ3OUbwGXtdHxuH',
-                        'TagaloBERTva_RoBERTa_hsf_30M.model':'https://drive.google.com/uc?id=1JozmqAIFE8pr5UeL6IYc4BNhpj-ep8NL',
+                        'TagaloBERTa_RoBERTa_hsf_30M.model':'https://drive.google.com/uc?id=1JozmqAIFE8pr5UeL6IYc4BNhpj-ep8NL',
                         'TagaloBERTa_hgfc_plus_Bi10_30M.model':'https://drive.google.com/uc?id=1GBI1GkSQvz5Hk8EOR7GFI2NGxY3rxETS',
                     }
 
